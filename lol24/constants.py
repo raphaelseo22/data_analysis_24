ac9c7318989ee40362c627e03d0b90eca31df06e
raphaelseo22 <seodh02@gmail.com>
2024-02-14 01:04:02 +0900

    Hide my API key

--- lol24/constants.py
+++ lol24/constants.py
@@ -1,4 +1,4 @@
-API_KEY = "RGAPI-c4898df2-41e8-4ce4-818c-32be22e961b3"
+API_KEY = "API_KEY"
 
 MATCH_ID_URL = "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=100&api_key={api_key}"
 MATCH_INFO_URL = (
